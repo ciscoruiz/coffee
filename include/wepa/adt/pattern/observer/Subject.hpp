d3805653c5e80d6386bffe0aa20a23df7838acc4
ciscoruiz <cisco.tierra@gmail.com>
2017-12-21 17:07:21 +0100

    Remove raw pointers from the observer pattern

--- include/wepa/adt/pattern/observer/Subject.hpp
+++ include/wepa/adt/pattern/observer/Subject.hpp
@@ -1,6 +1,6 @@
 // WEPA - Write Excellent Professional Applications
 //
-// (c) Copyright 2013 Francisco Ruiz Rayo
+//(c) Copyright 2013 Francisco Ruiz Rayo
 //
 // https://github.com/ciscoruiz/wepa
 //
@@ -23,11 +23,11 @@
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 // A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
 // OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT
 // LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // Author: cisco.tierra@gmail.com
@@ -35,8 +35,9 @@
 #ifndef _wepa_adt_pattern_observer_Subject_hpp
 #define _wepa_adt_pattern_observer_Subject_hpp
 
-#include <vector>
+#include <unordered_map>
 #include <set>
+#include <memory>
 
 #include <wepa/adt/RuntimeException.hpp>
 #include <wepa/adt/NamedObject.hpp>
@@ -54,52 +55,52 @@ namespace observer {
 class Observer;
 
 class Subject : public NamedObject {
-   static const Event nullEvent;
-
 public:
-   virtual ~Subject ();
+   static const Event AllEvents;
 
-   void subscribeObserver (Observer* observer, const Event& event = nullEvent) throw (RuntimeException);
-   void subscribeObserver (Observer* observer, const Event::Id eventId) throw (RuntimeException) {
+   virtual ~Subject();
+
+   void subscribeObserver(std::shared_ptr<Observer>& observer, const Event& event = AllEvents) throw(RuntimeException);
+   void subscribeObserver(std::shared_ptr<Observer>& observer, const Event::Id eventId) throw(RuntimeException) {
       subscribeObserver(observer, lookupEvent(eventId));
    }
-   bool unsubscribeObserver (Observer* observer) noexcept;
+   bool unsubscribeObserver(const std::string& observerName) noexcept;
 
-   size_t countObservers () const noexcept { return m_observers.size (); }
+   size_t countObservers() const noexcept { return m_observers.size(); }
 
-   virtual StreamString asString () const noexcept {
-      StreamString result ("pattern::observer::Subject {");
+   virtual StreamString asString() const noexcept {
+      StreamString result("pattern::observer::Subject {");
       return result << NamedObject::asString() << "}";
    }
 
 protected:
-   Subject (const std::string& name) : NamedObject(name) {;}
+   Subject(const std::string& name) : NamedObject(name) {;}
 
-   Event registerEvent (const Event::Id id) throw (RuntimeException);
-   Event lookupEvent (const Event::Id id) throw (RuntimeException);
+   Event registerEvent(const Event::Id id) throw(RuntimeException);
+   Event lookupEvent(const Event::Id id) throw(RuntimeException);
 
-   void notify (const Event::Id eventId) throw (RuntimeException) {
-      notify (lookupEvent(eventId));
+   void notify(const Event::Id eventId) throw(RuntimeException) {
+      notify(lookupEvent(eventId));
    }
-   void notify (const Event& event = nullEvent) noexcept;
+   void notify(const Event& event = AllEvents) noexcept;
 
-   typedef std::pair <Observer*, Event::BitMask> Subscription;
-   typedef std::vector <Subscription> Observers;
+   typedef std::pair<std::shared_ptr<Observer>, Event::BitMask> Subscription;
+   typedef std::unordered_map<std::string, Subscription> Observers;
    typedef Observers::iterator observer_iterator;
    Observers m_observers;
 
-   observer_iterator observer_begin () noexcept { return m_observers.begin (); }
-   observer_iterator observer_end () noexcept { return m_observers.end (); }
-   static Observer* get_observer (observer_iterator ii) { return ii->first; }
-   static Event::BitMask bitmask (observer_iterator ii) { return ii->second; }
+   observer_iterator observer_begin() noexcept { return m_observers.begin(); }
+   observer_iterator observer_end() noexcept { return m_observers.end(); }
+   static std::shared_ptr<Observer>& get_observer(observer_iterator ii) { return ii->second.first; }
+   static Event::BitMask bitmask(observer_iterator ii) { return ii->second.second; }
 
    typedef std::set <Event> Events;
    typedef Events::iterator event_iterator;
    Events m_events;
 
-   event_iterator event_begin () noexcept { return m_events.begin (); }
-   event_iterator event_end () noexcept { return m_events.end (); }
-   static const Event& event (event_iterator ii) { return *ii; }
+   event_iterator event_begin() noexcept { return m_events.begin(); }
+   event_iterator event_end() noexcept { return m_events.end(); }
+   static const Event& event(event_iterator ii) { return *ii; }
 };
 
 }
--- test/adt/PatternObserver_test.cc
+++ test/adt/PatternObserver_test.cc
@@ -1,6 +1,6 @@
 // WEPA - Write Excellent Professional Applications
 //
-// (c) Copyright 2013 Francisco Ruiz Rayo
+//(c) Copyright 2013 Francisco Ruiz Rayo
 //
 // https://github.com/ciscoruiz/wepa
 //
@@ -23,15 +23,16 @@
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 // A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
 // OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT
 // LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // Author: cisco.tierra@gmail.com
 //
+#include <wepa/adt/Millisecond.hpp>
 
 #include <wepa/adt/pattern/observer/Subject.hpp>
 #include <wepa/adt/pattern/observer/Event.hpp>
@@ -39,6 +40,7 @@
 
 #include <boost/test/unit_test.hpp>
 
+using namespace std;
 using namespace wepa::adt;
 using namespace wepa::adt::pattern;
 
@@ -49,7 +51,7 @@ class TheSubject : public observer::Subject {
 public:
    struct Events { enum _v { ChangeII = 0, ChangeSS, ChangeUU}; };
 
-   TheSubject () : observer::Subject ("TheSubject") {
+   TheSubject() : observer::Subject("TheSubject") {
       m_ii = 0;
       m_uu = 0;
       m_ss = initialValue;
@@ -59,26 +61,26 @@ public:
       m_events [Events::ChangeUU] = registerEvent(Events::ChangeUU);
    }
 
-   void registerTooManyEvents () throw (RuntimeException) {
-      for (int ii = 0; ii < 128; ++ ii)
+   void registerTooManyEvents() throw(RuntimeException) {
+      for(int ii = 0; ii < 128; ++ ii)
          registerEvent(ii);
    }
 
-   const observer::Event& getEvent (const Events::_v nn) const noexcept { return m_events [nn]; }
+   const observer::Event& getEvent(const Events::_v nn) const noexcept { return m_events [nn]; }
 
-   void setII (const int ii) noexcept { m_ii = ii; notify(m_events [Events::ChangeII]); }
-   void setSS (const std::string& ss) { m_ss = ss; notify(Events::ChangeSS); }
-   void setUU (const unsigned int uu) noexcept { m_uu = uu; notify(m_events [Events::ChangeUU]); }
+   void setII(const int ii) noexcept { m_ii = ii; notify(m_events [Events::ChangeII]); }
+   void setSS(const std::string& ss) { m_ss = ss; notify(Events::ChangeSS); }
+   void setUU(const unsigned int uu) noexcept { m_uu = uu; notify(m_events [Events::ChangeUU]); }
 
-   int getII () const noexcept { return m_ii; }
-   const std::string& getSS () const noexcept { return m_ss; }
-   unsigned int getUU () const noexcept { return m_uu; }
+   int getII() const noexcept { return m_ii; }
+   const std::string& getSS() const noexcept { return m_ss; }
+   unsigned int getUU() const noexcept { return m_uu; }
 
-   void clear () {
+   void clear() {
       m_ii = -1;
       m_ss = finalValue;
       m_uu = 0;
-      notify ();
+      notify(observer::Subject::AllEvents);
    }
 
 private:
@@ -90,232 +92,243 @@ private:
 
 class TheObserverII : public observer::Observer {
 public:
-   TheObserverII (TheSubject& subject) :
-      observer::Observer ("TheObserverII"), m_mySubject (subject), m_value (0)
-   {
-      subject.subscribeObserver(this, subject.getEvent(TheSubject::Events::ChangeII));
-   }
-
-   int getValue () const noexcept { return m_value; }
+   TheObserverII() : observer::Observer("TheObserverII"), m_value(0) {}
+   int getValue() const noexcept { return m_value; }
 
 private:
-   const TheSubject& m_mySubject;
    int m_value;
 
-   void update (const observer::Event& event) noexcept {
-      m_value = m_mySubject.getII ();
+   void update(const observer::Subject& subject, const observer::Event& event) noexcept {
+      m_value = static_cast<const TheSubject&>(subject).getII();
    }
 };
 
 class TheObserverSS : public observer::Observer {
 public:
-   TheObserverSS (TheSubject& subject) :
-      observer::Observer ("TheObserverSS"), m_mySubject (subject)
-   {
-      subject.subscribeObserver(this, TheSubject::Events::ChangeSS);
-   }
+   TheObserverSS() : observer::Observer("TheObserverSS") {}
 
-   const std::string& getValue () const noexcept { return m_value; }
+   const std::string& getValue() const noexcept { return m_value; }
 
 private:
-   const TheSubject& m_mySubject;
    std::string m_value;
 
-   void update (const observer::Event& event) noexcept {
-      m_value = m_mySubject.getSS ();
+   void update(const observer::Subject& subject, const observer::Event& event) noexcept {
+      m_value = static_cast<const TheSubject&>(subject).getSS();
    }
 };
 
 class TheNosyObserver : public observer::Observer {
 public:
-   TheNosyObserver (TheSubject& subject) :
-      observer::Observer ("TheNosyObserver"), m_mySubject (subject), m_ii (0), m_uu (0)
-   {
-      subject.subscribeObserver (this);
-   }
+   TheNosyObserver() : observer::Observer("TheNosyObserver"), m_ii(0), m_uu(0) {}
 
-   unsigned int getValue () const noexcept { return m_uu + m_ii; }
+   unsigned int getValue() const noexcept { return m_uu + m_ii; }
 
 private:
-   const TheSubject& m_mySubject;
    int m_ii;
    unsigned m_uu;
 
-   void update (const observer::Event& event) noexcept {
-      m_uu =  m_mySubject.getUU ();
-      m_ii = m_mySubject.getII ();
+   void update(const observer::Subject& subject, const observer::Event& event) noexcept {
+      auto mySubject = static_cast<const TheSubject&>(subject);
+      m_uu =  mySubject.getUU();
+      m_ii = mySubject.getII();
    }
 };
 
-class DualObserver : public observer::Observer {
+class OptimalObserver : public observer::Observer {
 public:
-   DualObserver (TheSubject& subject) :
-      observer::Observer ("DualObserver"), m_mySubject (subject), m_ii (0), m_uu (0)
-   {
-      auto ev1 = subject.getEvent(TheSubject::Events::ChangeII);
-      auto ev2 = subject.getEvent(TheSubject::Events::ChangeUU);
-      subject.subscribeObserver (this);
-   }
+   OptimalObserver() : observer::Observer("OptimalObserver") {}
 
-   int getII () const noexcept { return m_ii; }
-   unsigned int getUU () const noexcept { return m_uu; }
+   int getII() const noexcept { return m_ii.first; }
+   const Millisecond& getIITime() { return m_ii.second; }
+
+   unsigned int getUU() const noexcept { return m_uu.first; }
+   const Millisecond& getUUTime() { return m_ii.second; }
 
 private:
-   const TheSubject& m_mySubject;
-   int m_ii;
-   unsigned m_uu;
+   pair<int, Millisecond> m_ii;
+   pair<unsigned,Millisecond> m_uu;
 
-   void update (const observer::Event& event) noexcept {
-      m_uu =  m_mySubject.getUU ();
-      m_ii = m_mySubject.getII ();
+   void update(const observer::Subject& subject, const observer::Event& event) noexcept {
+      const TheSubject& mySubject = static_cast<const TheSubject&>(subject);
+
+      if (event.getId() == TheSubject::Events::ChangeUU) {
+         m_uu.first =  mySubject.getUU();
+         m_uu.second = Millisecond::getTime();
+      }
+      if (event.getId() == TheSubject::Events::ChangeII) {
+         m_ii.first = mySubject.getII();
+         m_ii.second = Millisecond::getTime();
+      }
    }
 };
 
-class DullObserver : public observer::Observer {
+class NullObserver : public observer::Observer {
 public:
-   DullObserver () : observer::Observer ("DullObserver") {;}
-private:
-   void update (const observer::Event& event) noexcept {
-   }
+   NullObserver() : observer::Observer("NullObserver") {;}
+   void update(const observer::Subject& subject, const observer::Event& event) noexcept {}
 };
 
-BOOST_AUTO_TEST_CASE (successful_register)
+BOOST_AUTO_TEST_CASE(successful_register)
 {
    TheSubject subject;
 
-   BOOST_REQUIRE_EQUAL (subject.getEvent (TheSubject::Events::ChangeII).getId (), 0);
-   BOOST_REQUIRE_EQUAL (subject.getEvent (TheSubject::Events::ChangeII).getBitMask(), 1);
-   BOOST_REQUIRE_EQUAL (subject.getEvent (TheSubject::Events::ChangeSS).getId (), 1);
-   BOOST_REQUIRE_EQUAL (subject.getEvent (TheSubject::Events::ChangeSS).getBitMask(), 2);
-   BOOST_REQUIRE_EQUAL (subject.getEvent (TheSubject::Events::ChangeUU).getId (), 2);
-   BOOST_REQUIRE_EQUAL (subject.getEvent (TheSubject::Events::ChangeUU).getBitMask(), 4);
+   BOOST_REQUIRE_EQUAL(subject.getEvent(TheSubject::Events::ChangeII).getId(), 0);
+   BOOST_REQUIRE_EQUAL(subject.getEvent(TheSubject::Events::ChangeII).getBitMask(), 1);
+   BOOST_REQUIRE_EQUAL(subject.getEvent(TheSubject::Events::ChangeSS).getId(), 1);
+   BOOST_REQUIRE_EQUAL(subject.getEvent(TheSubject::Events::ChangeSS).getBitMask(), 2);
+   BOOST_REQUIRE_EQUAL(subject.getEvent(TheSubject::Events::ChangeUU).getId(), 2);
+   BOOST_REQUIRE_EQUAL(subject.getEvent(TheSubject::Events::ChangeUU).getBitMask(), 4);
 }
 
-BOOST_AUTO_TEST_CASE (subscription_by_event)
+BOOST_AUTO_TEST_CASE(subscription_by_event)
 {
    TheSubject subject;
-   TheObserverII iiObserver (subject);
+   shared_ptr<TheObserverII> observer = make_shared<TheObserverII>();
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), 0);
+   shared_ptr<observer::Observer> base = observer;
+   subject.subscribeObserver(base, subject.getEvent(TheSubject::Events::ChangeII));
 
-   subject.setII (100);
+   BOOST_REQUIRE_EQUAL(observer->getValue(), 0);
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), 100);
+   subject.setII(100);
 
+   BOOST_REQUIRE_EQUAL(observer->getValue(), 100);
 }
 
-BOOST_AUTO_TEST_CASE (subscription_by_eventId)
+BOOST_AUTO_TEST_CASE(subscription_by_eventId)
 {
    TheSubject subject;
-   TheObserverSS ssObserver (subject);
+   shared_ptr<TheObserverSS> observer= make_shared<TheObserverSS>();
 
-   BOOST_REQUIRE_EQUAL (ssObserver.getValue (), initialValue);
+   shared_ptr<observer::Observer> base = observer;
+   subject.subscribeObserver(base, TheSubject::Events::ChangeSS);
 
-   subject.setSS ("Wow!");
+   BOOST_REQUIRE_EQUAL(observer->getValue(), initialValue);
 
-   BOOST_REQUIRE_EQUAL (ssObserver.getValue (), "Wow!");
+   subject.setSS("Wow!");
+
+   BOOST_REQUIRE_EQUAL(observer->getValue(), "Wow!");
 }
 
-BOOST_AUTO_TEST_CASE (nosy_subscription)
+BOOST_AUTO_TEST_CASE(nosy_subscription)
 {
    TheSubject subject;
-   TheObserverII iiObserver (subject);
-   TheNosyObserver nosyObserver (subject);
+   shared_ptr<TheObserverII> iiObserver = make_shared<TheObserverII>();
 
-   subject.setII (100);
+   shared_ptr<observer::Observer> base = iiObserver;
+   subject.subscribeObserver(base, TheSubject::Events::ChangeSS);
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), 100);
-   BOOST_REQUIRE_EQUAL (nosyObserver.getValue (), 100);
+   auto eventII = subject.getEvent(TheSubject::Events::ChangeII);
+   auto eventUU = subject.getEvent(TheSubject::Events::ChangeUU);
+   observer::Event event = eventII + eventUU;
+   shared_ptr<TheNosyObserver> nosyObserver = make_shared<TheNosyObserver>();
+   subject.subscribeObserver(base = nosyObserver, event);
 
-   subject.setUU (10);
+   subject.setII(100);
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), 100);
-   BOOST_REQUIRE_EQUAL (nosyObserver.getValue (), 110);
+   BOOST_REQUIRE_EQUAL(iiObserver->getValue(), 100);
+   BOOST_REQUIRE_EQUAL(nosyObserver->getValue(), 100);
+
+   subject.setUU(10);
+
+   BOOST_REQUIRE_EQUAL(iiObserver->getValue(), 100);
+   BOOST_REQUIRE_EQUAL(nosyObserver->getValue(), 110);
 }
 
-BOOST_AUTO_TEST_CASE (full_update)
+BOOST_AUTO_TEST_CASE(full_update)
 {
    TheSubject subject;
-   TheObserverII iiObserver (subject);
-   TheObserverSS ssObserver (subject);
-   TheNosyObserver nosyObserver (subject);
+   shared_ptr<TheObserverSS> ssObserver = make_shared<TheObserverSS>();
+   shared_ptr<TheObserverII> iiObserver = make_shared<TheObserverII>();
 
-   BOOST_REQUIRE_EQUAL (ssObserver.getValue (), initialValue);
+   shared_ptr<observer::Observer> base = ssObserver;
+   subject.subscribeObserver(base, TheSubject::Events::ChangeSS);
 
-   subject.setII (100);
+   subject.subscribeObserver(base = iiObserver, TheSubject::Events::ChangeII);
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), 100);
-   BOOST_REQUIRE_EQUAL (nosyObserver.getValue (), 100);
+   auto eventII = subject.getEvent(TheSubject::Events::ChangeII);
+   auto eventUU = subject.getEvent(TheSubject::Events::ChangeUU);
+   observer::Event event = eventII + eventUU;
+   shared_ptr<TheNosyObserver> nosyObserver = make_shared<TheNosyObserver>();
+   subject.subscribeObserver(base = nosyObserver, event);
 
-   subject.setUU (10);
+   BOOST_REQUIRE_EQUAL(ssObserver->getValue(), initialValue);
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), 100);
-   BOOST_REQUIRE_EQUAL (nosyObserver.getValue (), 110);
+   subject.setII(100);
 
-   subject.clear ();
+   BOOST_REQUIRE_EQUAL(iiObserver->getValue(), 100);
+   BOOST_REQUIRE_EQUAL(nosyObserver->getValue(), 100);
 
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue (), -1);
-   BOOST_REQUIRE_EQUAL (ssObserver.getValue (), finalValue);
-   BOOST_REQUIRE_EQUAL (nosyObserver.getValue (), 4294967295U);
+   subject.setUU(50);
+
+   BOOST_REQUIRE_EQUAL(iiObserver->getValue(), 100);
+   BOOST_REQUIRE_EQUAL(nosyObserver->getValue(), 150);
+
+   subject.clear();
+
+   BOOST_REQUIRE_EQUAL(iiObserver->getValue(), -1);
+   BOOST_REQUIRE_EQUAL(ssObserver->getValue(), finalValue);
+   BOOST_REQUIRE_EQUAL(nosyObserver->getValue(), 4294967295U);
 }
 
-
-BOOST_AUTO_TEST_CASE (dual_subscription)
+BOOST_AUTO_TEST_CASE(optimal_subscription)
 {
    TheSubject subject;
-   DualObserver the_observer (subject);
+   shared_ptr<OptimalObserver> observer = make_shared<OptimalObserver>();
 
-   subject.setII (100);
+   shared_ptr<observer::Observer> base = observer;
+   auto eventII = subject.getEvent(TheSubject::Events::ChangeII);
+   auto eventUU = subject.getEvent(TheSubject::Events::ChangeUU);
+   observer::Event event = eventII + eventUU;
+   subject.subscribeObserver(base, event);
+
+   subject.setII(100);
+
+   usleep(1100);
+
    subject.setUU(3000);
 
-   BOOST_REQUIRE_EQUAL (the_observer.getII(), 100);
-   BOOST_REQUIRE_EQUAL (the_observer.getUU(), 3000);
-
-   subject.setUU (10003000);
-   BOOST_REQUIRE_EQUAL (the_observer.getUU(), 10003000);
+   BOOST_REQUIRE_EQUAL(observer->getII(), 100);
+   BOOST_REQUIRE_EQUAL(observer->getUU(), 3000);
+   BOOST_REQUIRE_LT(observer->getIITime(), observer->getUUTime());
 }
 
-BOOST_AUTO_TEST_CASE (too_many_events)
+BOOST_AUTO_TEST_CASE(too_many_events)
 {
    TheSubject subject;
 
-   BOOST_REQUIRE_THROW (subject.registerTooManyEvents(), wepa::adt::RuntimeException);
+   BOOST_REQUIRE_THROW(subject.registerTooManyEvents(), wepa::adt::RuntimeException);
 }
 
-BOOST_AUTO_TEST_CASE (promicuous_observer)
+BOOST_AUTO_TEST_CASE(promicuous_observer)
 {
    TheSubject subject1;
    TheSubject subject2;
 
-   DualObserver theObserver (subject1);
+   shared_ptr<NullObserver> observer = make_shared<NullObserver>();
 
-   BOOST_REQUIRE_THROW (subject2.subscribeObserver(&theObserver), wepa::adt::RuntimeException);
+   shared_ptr<observer::Observer> base = observer;
+   subject1.subscribeObserver(base, TheSubject::Events::ChangeII);
+
+   BOOST_REQUIRE_THROW(subject2.subscribeObserver(base, TheSubject::Events::ChangeII), wepa::adt::RuntimeException);
 }
 
-BOOST_AUTO_TEST_CASE (observer_out_of_scope)
+BOOST_AUTO_TEST_CASE(observer_out_of_scope)
 {
-   TheSubject subject;
-
-   TheObserverII iiObserver (subject);
+   shared_ptr<NullObserver> observer = make_shared<NullObserver>();
 
    if (true) {
-      DualObserver dualObserver (subject);
+      TheSubject subject;
 
-      BOOST_REQUIRE_EQUAL(subject.countObservers(), 2);
+      shared_ptr<observer::Observer> base = observer;
+      subject.subscribeObserver(base, TheSubject::Events::ChangeII);
 
-      subject.setII (1010);
-      subject.setUU (3333);
+      BOOST_REQUIRE_EQUAL(subject.countObservers(), 1);
 
-      BOOST_REQUIRE_EQUAL (dualObserver.getII(), 1010);
-      BOOST_REQUIRE_EQUAL (dualObserver.getUU(), 3333);
 
-      BOOST_REQUIRE_EQUAL (iiObserver.getValue(), 1010);
+      BOOST_REQUIRE_EQUAL(observer->isSubscribed(), true);
    }
 
-   BOOST_REQUIRE_EQUAL(subject.countObservers(), 1);
-
-   subject.setII (2020);
-
-   BOOST_REQUIRE_EQUAL (iiObserver.getValue(), 2020);
+   BOOST_REQUIRE_EQUAL(observer->isSubscribed(), false);
 }
 

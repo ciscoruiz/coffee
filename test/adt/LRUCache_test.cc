51b382915f4a6d93389bb8276097537cdcfad165
Francisco Ruiz <cisco.tierra@gmail.com>
2018-01-12 09:19:17 +0100

    Unit test for LRU cache

--- test/adt/LRUCache_test.cc
+++ test/adt/LRUCache_test.cc
@@ -39,7 +39,77 @@
 
 using namespace wepa::adt::pattern;
 
-BOOST_AUTO_TEST_CASE(basic_access)
+BOOST_AUTO_TEST_CASE(lrucache_insert_twice)
+{
+   const int MaxCacheSize = 5;
+
+   typedef lru::Cache<int, wepa::adt::StreamString> TheCache;
+
+   TheCache cache(MaxCacheSize);
+
+   cache.set(0, "text");
+   BOOST_REQUIRE_EQUAL(cache.size(), 1);
+
+   cache.set(0, "other");
+   BOOST_REQUIRE_EQUAL(cache.size(), 1);
+
+   BOOST_REQUIRE_EQUAL(cache.value(cache.begin()), "other");
+}
+
+BOOST_AUTO_TEST_CASE(lrucache_find)
+{
+   const int MaxCacheSize = 5;
+
+   typedef lru::Cache<int, wepa::adt::StreamString> TheCache;
+
+   TheCache cache(MaxCacheSize);
+
+   TheCache::pair_iterator ii = cache.find(0);
+   BOOST_REQUIRE_EQUAL(ii == cache.end(), true);
+
+   for(int ii = 0; ii < MaxCacheSize; ++ ii) {
+      wepa::adt::StreamString string;
+      string << "Number-" << ii;
+      cache.set(ii, string);
+   }
+
+   for(int ii = 0; ii < MaxCacheSize; ++ ii) {
+      TheCache::pair_iterator ww = cache.find(ii);
+      BOOST_REQUIRE_EQUAL(ww == cache.end(), false);
+   }
+}
+
+BOOST_AUTO_TEST_CASE(lrucache_erase)
+{
+   const int MaxCacheSize = 5;
+
+   typedef lru::Cache<int, wepa::adt::StreamString> TheCache;
+
+   TheCache cache(MaxCacheSize);
+
+   for(int ii = 0; ii < MaxCacheSize; ++ ii) {
+      wepa::adt::StreamString string;
+      string << "Number-" << ii;
+      cache.set(ii, string);
+   }
+
+   BOOST_REQUIRE_EQUAL(cache.erase(3), true);
+   BOOST_REQUIRE_EQUAL(cache.size(), MaxCacheSize - 1);
+
+   BOOST_REQUIRE_EQUAL(cache.erase(3), false);
+   BOOST_REQUIRE_EQUAL(cache.size(), MaxCacheSize - 1);
+
+   BOOST_REQUIRE_EQUAL(cache.erase(0), true);
+   BOOST_REQUIRE_EQUAL(cache.size(), MaxCacheSize - 2);
+
+   std::vector<int> expectedOrder = { 1, 2, 4 };
+   int jj = 0;
+   for (TheCache::pair_iterator ii = cache.begin(), maxii = cache.end(); ii != maxii; ++ ii) {
+      BOOST_REQUIRE_EQUAL(TheCache::key(ii), expectedOrder[jj ++]);
+   }
+}
+
+BOOST_AUTO_TEST_CASE(lrucache_max_size)
 {
    const int MaxCacheSize = 5;
 
@@ -53,6 +123,7 @@ BOOST_AUTO_TEST_CASE(basic_access)
       cache.set(ii, string);
    }
 
+   BOOST_REQUIRE_EQUAL(cache.find(0) == cache.end(), true);
    BOOST_REQUIRE_EQUAL(cache.size(), MaxCacheSize);
 
    int jj = 1;
@@ -65,7 +136,79 @@ BOOST_AUTO_TEST_CASE(basic_access)
    }
 }
 
+BOOST_AUTO_TEST_CASE(lrucache_update)
+{
+   const int MaxCacheSize = 5;
+
+   typedef lru::Cache<int, wepa::adt::StreamString> TheCache;
+
+   TheCache cache(MaxCacheSize);
+
+   for(int ii = 0; ii < MaxCacheSize; ++ ii) {
+      wepa::adt::StreamString string;
+      string << "Number-" << ii;
+      cache.set(ii, string);
+   }
+
+   cache.set(0, "new text");
+
+   std::vector<int> expectedOrder = { 1, 2, 3, 4, 0 };
+   int jj = 0;
+   for (TheCache::pair_iterator ii = cache.begin(), maxii = cache.end(); ii != maxii; ++ ii) {
+      BOOST_REQUIRE_EQUAL(TheCache::key(ii), expectedOrder[jj ++]);
+   }
+}
+
+BOOST_AUTO_TEST_CASE(lrucache_refresh)
+{
+   const int MaxCacheSize = 5;
+
+   typedef lru::Cache<int, wepa::adt::StreamString> TheCache;
+
+   TheCache cache(MaxCacheSize);
+
+   for(int ii = 0; ii < MaxCacheSize; ++ ii) {
+      wepa::adt::StreamString string;
+      string << "Number-" << ii;
+      cache.set(ii, string);
+   }
+
+   cache.find(0);
+
+   std::vector<int> expectedOrder = { 1, 2, 3, 4, 0 };
+   int jj = 0;
+   for (TheCache::pair_iterator ii = cache.begin(), maxii = cache.end(); ii != maxii; ++ ii) {
+      BOOST_REQUIRE_EQUAL(TheCache::key(ii), expectedOrder[jj ++]);
+   }
+}
 
 
+BOOST_AUTO_TEST_CASE(lrucache_shared_ptr)
+{
+   typedef std::pair<int,int> Complex;
+   typedef std::shared_ptr<Complex> Key;
 
+   const int MaxFirstLevel = 5;
+   const int MaxSecondLevel = 5;
 
+   typedef lru::Cache<Key, wepa::adt::StreamString> TheCache;
+
+   TheCache cache(MaxFirstLevel * MaxSecondLevel);
+
+   for(int ii = 0; ii < MaxFirstLevel; ++ ii) {
+      for (int jj = 0; jj < MaxSecondLevel; ++ jj) {
+         Key key = std::make_shared<Complex>(ii, jj);
+         wepa::adt::StreamString string;
+         string << ii << "-" << jj;
+         cache.set(key, string);
+      }
+   }
+
+   cache.find(0);
+
+   std::vector<int> expectedOrder = { 1, 2, 3, 4, 0 };
+   int jj = 0;
+   for (TheCache::pair_iterator ii = cache.begin(), maxii = cache.end(); ii != maxii; ++ ii) {
+      BOOST_REQUIRE_EQUAL(TheCache::key(ii), expectedOrder[jj ++]);
+   }
+}

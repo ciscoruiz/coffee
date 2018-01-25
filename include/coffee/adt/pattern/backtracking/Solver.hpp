db5e2ffe2bfafd7ac747198e32666f52e0790537
Francisco Ruiz <cisco.tierra@gmail.com>
2018-01-25 11:45:41 +0100

    Refactoring for coffee.adt.pattern.backtracking
    Avoid the use of raw pointers and boost classes

--- include/coffee/adt/pattern/backtracking/Solver.hpp
+++ include/coffee/adt/pattern/backtracking/Solver.hpp
@@ -1,6 +1,6 @@
 // COFFEE - COmpany eFFEEctive Platform
 //
-// (c) Copyright 2018 Francisco Ruiz Rayo
+//(c) Copyright 2018 Francisco Ruiz Rayo
 //
 // https://github.com/ciscoruiz/coffee
 //
@@ -23,11 +23,11 @@
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 // A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
 // OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT
 // LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // Author: cisco.tierra@gmail.com
@@ -54,87 +54,84 @@ namespace backtracking {
  * \see http://en.wikipedia.org/wiki/Backtracking
  * \see http://www.cs.uiuc.edu/~jeffe/teaching/algorithms/notes/02-backtracking.pdf
  */
-template <typename _T> class Solver : public TreeNode <_T> {
+template <typename _T> class Solver : public TreeNode<_T> {
 public:
    typedef TreeNode <_T> Solution;
+   typedef typename std::shared_ptr<Solution> shared_solution;
 
    /**
     * Constructor.
     */
-   Solver () {;}
+   Solver() {;}
 
    /**
     * Destructor.
     */
-   virtual ~Solver () { ; }
+   virtual ~Solver() { ; }
 
    /**
     *
     * @param problem
     * @return Solutions for the received problem. It will erased when the Solver goes out of scope.
     */
-   bool apply () noexcept {
-      this->clear ();
+   bool apply() noexcept {
+      this->clear();
 
-      Solution* solution = NULL;
 
-      for (_T ii = first (0, getStartingPoint()); stop (0, ii) == false; ii = next (0, ii)) {
-         solution = new Solution (ii);
+      for(_T ii = first(0, getStartingPoint()); stop(0, ii) == false; ii = next(0, ii)) {
+         shared_solution solution = std::make_shared<Solution>(ii);
 
-         if (exploreSolutions (1, solution) == true)
-            this->add (solution);
-         else
-            delete solution;
+         if(exploreSolutions(1, solution) == true)
+            this->add(solution);
       }
 
       return this->hasSuccessor();
    }
 
-   const Solution* getSolution (const int index) const throw (RuntimeException) { return this->getNextStep(index); }
+   shared_solution getSolution(const int index) const throw(RuntimeException) { return this->getNextStep(index); }
 
 protected:
-   virtual _T getStartingPoint () const noexcept = 0;
+   virtual _T getStartingPoint() const noexcept = 0;
 
-   virtual _T first (const int depth, _T value) const noexcept = 0;
-   virtual bool stop (const int depth, _T value) const noexcept = 0;
-   virtual _T next (const int depth, _T value) const noexcept = 0;
+   virtual _T first(const int depth, _T value) const noexcept = 0;
+   virtual bool stop(const int depth, _T value) const noexcept = 0;
+   virtual _T next(const int depth, _T value) const noexcept = 0;
 
-   virtual bool reject (const Solution* solution, const _T candidate) const noexcept = 0;
-   virtual bool accept (const Solution* solution, const _T candidate) const noexcept = 0;
+   virtual bool reject(const shared_solution& solution, const _T candidate) const noexcept = 0;
+   virtual bool accept(const shared_solution& solution, const _T candidate) const noexcept = 0;
 
 private:
-
-   bool backtracking (const int depth, Solution* onTestSolution, const _T candidate) noexcept {
-      if (reject (onTestSolution, candidate) == true) return false;
-
-      // onTestSolution + candidate become to a real Solution
-      if (accept (onTestSolution, candidate) == true) {
-         onTestSolution->add (new Solution (candidate, onTestSolution));
-         return true;
-      }
-
-      Solution* partialSolution = new Solution (candidate, onTestSolution);
-
-      if (exploreSolutions (depth + 1, partialSolution) == false) {
-         delete partialSolution;
-         return false;
-      }
-
-      onTestSolution->add (partialSolution);
-
-      return true;
-   }
-
-   bool exploreSolutions (const int depth, Solution* onTestSolution) throw  () {
+   bool exploreSolutions(const int depth, shared_solution& onTestSolution) throw () {
       bool hasSolution = false;
 
-      for (_T ii = first (depth, onTestSolution->getValue ()); stop (depth, ii) == false; ii = next (depth, ii)) {
-         if (backtracking (depth, onTestSolution, ii) == true)
+      for(_T ii = first(depth, onTestSolution->getValue()); stop(depth, ii) == false; ii = next(depth, ii)) {
+         if(backtracking(depth, onTestSolution, ii) == true)
             hasSolution = true;
       }
 
       return hasSolution;
    }
+
+   bool backtracking(const int depth, shared_solution& onTestSolution, const _T candidate) noexcept {
+      if(reject(onTestSolution, candidate) == true) return false;
+
+      // onTestSolution + candidate become to a real Solution
+      if(accept(onTestSolution, candidate) == true) {
+         onTestSolution->add(std::make_shared<Solution>(candidate, onTestSolution));
+         return true;
+      }
+
+      shared_solution partialSolution = std::make_shared<Solution>(candidate, onTestSolution);
+
+      if(exploreSolutions(depth + 1, partialSolution) == false) {
+         return false;
+      }
+
+      onTestSolution->add(partialSolution);
+
+      return true;
+   }
+
 };
 
 }
--- include/coffee/adt/pattern/backtracking/TreeNode.hpp
+++ include/coffee/adt/pattern/backtracking/TreeNode.hpp
@@ -1,6 +1,6 @@
 // COFFEE - COmpany eFFEEctive Platform
 //
-// (c) Copyright 2018 Francisco Ruiz Rayo
+//(c) Copyright 2018 Francisco Ruiz Rayo
 //
 // https://github.com/ciscoruiz/coffee
 //
@@ -23,11 +23,11 @@
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 // A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
 // OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT
 // LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // Author: cisco.tierra@gmail.com
@@ -36,7 +36,8 @@
 #ifndef _coffee_adt_pattern_backtracking_Step_hpp_
 #define _coffee_adt_pattern_backtracking_Step_hpp_
 
-#include <boost/ptr_container/ptr_vector.hpp>
+#include <memory>
+#include <vector>
 
 #include <coffee/adt/RuntimeException.hpp>
 
@@ -49,7 +50,8 @@ namespace pattern {
 namespace backtracking {
 
 template <typename _T> class TreeNode {
-   typedef boost::ptr_vector <TreeNode <_T> > Successors;
+   typedef typename std::shared_ptr<TreeNode<_T> > shared_tree_node;
+   typedef std::vector<shared_tree_node> Successors;
 
 public:
    typedef typename Successors::iterator successor_iterator;
@@ -65,60 +67,60 @@ public:
        * @param treeNode
        * @param depth
        */
-      virtual ~Predicate () {;}
+      virtual ~Predicate() {;}
 
       /**
        * Apply the predicate on the received treeNode.
        * @param treeNode One solution's treeNode
        * @param depth Depth of this treeNode.
        */
-      virtual void apply (const TreeNode <_T>& treeNode, const int depth) const noexcept = 0;
+      virtual void apply(const TreeNode<_T>& treeNode, const int depth) const noexcept = 0;
    };
 
-   TreeNode (const _T& value) : m_value (value), m_predeccesor (NULL) {;}
-   TreeNode (const _T& value, const TreeNode* predeccesor) :  m_value (value), m_predeccesor (predeccesor) {;}
-   virtual ~TreeNode () { m_successors.clear (); }
+   TreeNode(const _T& value) : m_value(value){;}
+   TreeNode(const _T& value, const shared_tree_node& predeccesor) :  m_value(value), m_predeccesor(predeccesor) {;}
+   virtual ~TreeNode() { m_successors.clear(); }
 
-   void add (TreeNode <_T>* treeNode) noexcept { m_successors.push_back (treeNode); }
+   void add(const shared_tree_node& treeNode) noexcept { m_successors.push_back(treeNode); }
 
-   const _T& getValue () const noexcept { return m_value; }
+   const _T& getValue() const noexcept { return m_value; }
 
-   successor_iterator successor_begin () noexcept { return m_successors.begin (); }
-   successor_iterator successor_end () noexcept { return m_successors.end (); }
-   static TreeNode<_T>& get_successor (successor_iterator ii) noexcept{ return *ii; }
+   successor_iterator successor_begin() noexcept { return m_successors.begin(); }
+   successor_iterator successor_end() noexcept { return m_successors.end(); }
+   static shared_tree_node& get_successor(successor_iterator ii) noexcept { return std::ref(*ii); }
 
-   const_successor_iterator successor_begin () const noexcept { return m_successors.begin (); }
-   const_successor_iterator successor_end () const noexcept { return m_successors.end (); }
-   static const TreeNode<_T>& get_successor (const_successor_iterator ii) noexcept{ return *ii; }
+   const_successor_iterator successor_begin() const noexcept { return m_successors.begin(); }
+   const_successor_iterator successor_end() const noexcept { return m_successors.end(); }
+   static const shared_tree_node& get_successor(const_successor_iterator ii) noexcept { return std::ref(*ii); }
 
-   size_t successors_size () const noexcept { return m_successors.size (); }
+   size_t successors_size() const noexcept { return m_successors.size(); }
 
-   bool hasSuccessor () const noexcept { return m_successors.empty () == false; }
+   bool hasSuccessor() const noexcept { return m_successors.empty() == false; }
 
-   const TreeNode<_T>* getPredeccessor () const noexcept { return m_predeccesor; }
+   const shared_tree_node getPredeccessor() const noexcept { return m_predeccesor; }
    
    /**
     * Runs over tree with depth-first algorithm and apply the \em _Predicate on every node.
     * \param predicate Operation to apply on every Node.
     * \param depth Depth of this node
     */
-   virtual void depthFirst (const Predicate& predicate, const int depth = 0) const noexcept {
-      predicate.apply (*this, depth);
+   virtual void depthFirst(const Predicate& predicate, const int depth = 0) const noexcept {
+      predicate.apply(*this, depth);
 
-      for (const_successor_iterator ii = successor_begin (), maxii = successor_end (); ii != maxii; ++ ii) {
-         get_successor (ii).depthFirst (predicate, depth + 1);
+      for(const_successor_iterator ii = successor_begin(), maxii = successor_end(); ii != maxii; ++ ii) {
+         get_successor(ii)->depthFirst(predicate, depth + 1);
       }
    }
 
    /**
-    * @return The number of found solutions (which matches with the number of leaf nodes on the tree)
+    * @return The number of found solutions(which matches with the number of leaf nodes on the tree)
     */
-   size_t countSolutions () const noexcept {
+   size_t countSolutions() const noexcept {
       int result = 0;
 
-      if (hasSuccessor() == true) {
-         for (const_successor_iterator ii = successor_begin (), maxii = successor_end (); ii != maxii; ++ ii) {
-            result += get_successor (ii).countSolutions ();
+      if(hasSuccessor() == true) {
+         for(const_successor_iterator ii = successor_begin(), maxii = successor_end(); ii != maxii; ++ ii) {
+            result += get_successor(ii)->countSolutions();
          }
       }
       else
@@ -127,24 +129,24 @@ public:
       return result;
    }
 
-   const TreeNode<_T>* getNextStep (const int index) const
-      throw (RuntimeException)
+   const shared_tree_node getNextStep(const int index) const
+      throw(RuntimeException)
    {
-      if (m_successors.size () <= index) {
-         COFFEE_THROW_EXCEPTION ("Index=" << index << " is out of range (" << m_successors.size () << ")");
+      if(m_successors.size() <= index) {
+         COFFEE_THROW_EXCEPTION("Index=" << index << " is out of range(" << m_successors.size() << ")");
       }
 
-      return &get_successor (m_successors.begin () + index);
+      return get_successor(m_successors.begin() + index);
    }
 
 protected:
-   TreeNode () : m_predeccesor (NULL) {;}
+   TreeNode() : m_predeccesor(NULL) {;}
 
-   void clear () noexcept { m_successors.clear (); }
+   void clear() noexcept { m_successors.clear(); }
 
 private:
    _T m_value;
-   const TreeNode<_T>* m_predeccesor;
+   shared_tree_node  m_predeccesor;
    Successors m_successors;
 };
 

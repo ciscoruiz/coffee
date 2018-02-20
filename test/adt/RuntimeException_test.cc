bfe7b28816753ff047c435feaef185977e8c809e
Francisco Ruiz <cisco.tierra@gmail.com>
2018-02-20 16:02:43 +0100

    Includes MIT License

--- test/adt/RuntimeException_test.cc
+++ test/adt/RuntimeException_test.cc
@@ -1,3 +1,26 @@
+// MIT License
+// 
+// Copyright (c) 2018 Francisco Ruiz (francisco.ruiz.rayo@gmail.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
 #include <boost/test/unit_test.hpp>
 
 #include <coffee/adt/RuntimeException.hpp>
@@ -39,7 +62,7 @@ BOOST_AUTO_TEST_CASE( RuntimeException_asString )
    }
    catch (adt::RuntimeException& ex) {
       BOOST_REQUIRE ( strcmp ("this is goo", ex.what ()) == 0 );
-      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(43): int goo()] this is goo") != std::string::npos);
+      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(32): int goo()] this is goo") != std::string::npos);
    }
 
    AAA aaa;
@@ -48,7 +71,7 @@ BOOST_AUTO_TEST_CASE( RuntimeException_asString )
       aaa.member (10, 'z');
    }
    catch (adt::RuntimeException& ex) {
-      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(62): void AAA::member(int, char)] xx:10 zz:z") != std::string::npos);
+      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(51): void AAA::member(int, char)] xx:10 zz:z") != std::string::npos);
       BOOST_REQUIRE(strcmp ("xx:10 zz:z", ex.what ()) == 0 );
    }
 
@@ -56,7 +79,7 @@ BOOST_AUTO_TEST_CASE( RuntimeException_asString )
       AAA::member2 (11.11);
    }
    catch (adt::RuntimeException& ex) {
-      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(65): static void AAA::member2(float)] float: 11.110000") != std::string::npos);
+      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(54): static void AAA::member2(float)] float: 11.110000") != std::string::npos);
       BOOST_REQUIRE(strcmp ("float: 11.110000", ex.what ()) == 0 );
    }
 }
@@ -73,7 +96,7 @@ BOOST_AUTO_TEST_CASE( RuntimeException_errorCode )
       hoo ();
    }
    catch (adt::RuntimeException& ex) {
-      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(99): void hoo()] ErrorCode: 100 | this is hoo") != std::string::npos);
+      BOOST_REQUIRE(ex.asString ().find("RuntimeException_test.cc(88): void hoo()] ErrorCode: 100 | this is hoo") != std::string::npos);
       BOOST_REQUIRE(strcmp ("this is hoo", ex.what ()) == 0 );
    }
 }

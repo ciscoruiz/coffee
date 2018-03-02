d93cc47bdb50f015ae5786d30a33dd59c91a24f0
Francisco Ruiz <cisco.tierra@gmail.com>
2018-03-02 21:48:17 +0100

    Bumping version release-18.03.02

--- source/config/config.SCCS.cc
+++ source/config/config.SCCS.cc
@@ -25,7 +25,7 @@
 #include <coffee/config/SCCS.hpp>
 #include <coffee/config/SCCSRepository.hpp>
 
-coffee_sccs_define_tag (config,release-18.02.26);
+coffee_sccs_define_tag (config,release-18.03.02);
 
 using namespace coffee;
 

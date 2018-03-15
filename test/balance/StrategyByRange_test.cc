c38d0b3f44676beece0f0a4f5011b52d53511028
Francisco Ruiz <cisco.tierra@gmail.com>
2018-03-15 13:33:25 +0100

    Improve coverage for test::balance

--- test/balance/StrategyByRange_test.cc
+++ test/balance/StrategyByRange_test.cc
@@ -49,39 +49,6 @@ using namespace coffee;
 using namespace coffee::balance;
 using namespace coffee::test::balance;
 
-namespace ByRangeTest {
-   typedef std::map <int, int> CounterContainer;
-   typedef CounterContainer::iterator counter_iterator;
-
-   static int MaxResources = 13;
-
-   void incrementUse (CounterContainer& counterContainer, const std::shared_ptr<Resource>& resource)
-   {
-      std::shared_ptr<TestResource> myResource = TestResource::cast(resource);
-
-      counter_iterator cc = counterContainer.find (myResource->getKey());
-
-      if (cc == counterContainer.end ()) {
-         counterContainer [myResource->getKey()] = 1;
-      }
-      else {
-         cc->second ++;
-      }
-   }
-
-   void parallel_work(std::mutex& mutexContainer, CounterContainer& counterContainer, balance::StrategyByRange& strategy)
-   {
-      for (int ii = 0; ii < 1000; ++ ii) {
-         auto resource = strategy.apply (ii);
-
-         if (true) {
-            std::lock_guard <std::mutex> guard (mutexContainer);
-            incrementUse (counterContainer, resource);
-         }
-      }
-   }
-}
-
 BOOST_AUTO_TEST_CASE(byrange_empty_strategy)
 {
    balance::StrategyByRange mainStrategy;
@@ -121,19 +88,19 @@ BOOST_AUTO_TEST_CASE(byrange_sharing)
    balance::StrategyByRange mainStrategy;
 
    {
-      auto resourceList = ResourceListFixture::setup(ByRangeTest::MaxResources, 0);
+      auto resourceList = ResourceListFixture::setup(ResourceListFixture::MaxResources, 0);
       std::shared_ptr<balance::StrategyRoundRobin> strategy = std::make_shared<balance::StrategyRoundRobin>(resourceList);
       mainStrategy.addRange(100, 200, strategy);
    }
 
    {
-      auto resourceList = ResourceListFixture::setup(ByRangeTest::MaxResources, 100);
+      auto resourceList = ResourceListFixture::setup(ResourceListFixture::MaxResources, 100);
       std::shared_ptr<balance::StrategyRoundRobin> strategy = std::make_shared<balance::StrategyRoundRobin>(resourceList);
       mainStrategy.addRange(205, 350, strategy);
    }
 
    {
-      auto resourceList = ResourceListFixture::setup(ByRangeTest::MaxResources, 1000);
+      auto resourceList = ResourceListFixture::setup(ResourceListFixture::MaxResources, 1000);
       std::shared_ptr<balance::StrategyRoundRobin> strategy = std::make_shared<balance::StrategyRoundRobin>(resourceList);
       mainStrategy.addRange(351, 450, strategy);
    }
@@ -148,84 +115,3 @@ BOOST_AUTO_TEST_CASE(byrange_sharing)
 
    BOOST_REQUIRE_THROW(mainStrategy.apply (2000), balance::ResourceUnavailableException);
 }
-
-/*
-
-BOOST_AUTO_TEST_CASE( byrange_balance_quality)
-{
-   using namespace ByRangeTest;
-
-   balance::ByRange myBalance;
-
-   BOOST_REQUIRE_THROW (myBalance.add (NULL), adt::RuntimeException);
-
-   BOOST_REQUIRE_EQUAL(myBalance.add (myBalance.get (0)), false);
-
-   myBalance.initialize();
-
-   CounterContainer counterContainer;
-
-   for (int ii = 0; ii < balance::ByRange::MaxResources * balance::ByRange::MaxLoop; ++ ii){
-      incrementUse (counterContainer, myBalance.apply(ii));
-   }
-
-   BOOST_REQUIRE_EQUAL (counterContainer.size (), balance::ByRange::MaxResources);
-
-   for (counter_iterator ii = counterContainer.begin (), maxii = counterContainer.end (); ii != maxii; ++ ii) {
-      BOOST_REQUIRE_EQUAL (ii->second, balance::ByRange::MaxLoop);
-   }
-}
-
-BOOST_AUTO_TEST_CASE( byrange_balance_dont_use_unavailables )
-{
-   balance::ByRange myBalance;
-
-   myBalance.initialize();
-
-   Resource* resource1 = myBalance.apply(5);
-   Resource* resource2 = myBalance.apply(15);
-
-   BOOST_REQUIRE_EQUAL (resource1, resource2);
-
-   myBalance.get (5)->setAvailable(false);
-   myBalance.get (6)->setAvailable(false);
-
-   resource1 = myBalance.apply(5);
-
-   BOOST_REQUIRE_EQUAL (resource1, myBalance.get (7));
-
-   for (int ii = 0; ii < balance::ByRange::MaxResources; ++ ii) {
-      myBalance.get(ii)->setAvailable(false);
-   }
-
-   BOOST_REQUIRE_THROW (myBalance.apply(2), adt::RuntimeException);
-}
-
-BOOST_AUTO_TEST_CASE( byrange_balance_multithread )
-{
-   using namespace ByRangeTest;
-
-   balance::ByRange myBalance;
-   std::mutex mutexContainer;
-   CounterContainer counterContainer;
-
-   myBalance.initialize();
-
-   std::thread t1(do_work, std::ref (mutexContainer), std::ref (counterContainer), std::ref (myBalance));
-   std::thread t2(do_work, std::ref (mutexContainer), std::ref (counterContainer), std::ref (myBalance));
-   std::thread t3(do_work, std::ref (mutexContainer), std::ref (counterContainer), std::ref (myBalance));
-
-   t1.join ();
-   t2.join ();
-   t3.join ();
-
-   int sumUse = 0;
-
-   for (auto& useCounter : counterContainer) {
-      BOOST_REQUIRE_GT (useCounter.second, 0);
-      sumUse += useCounter.second;
-   }
-
-   BOOST_REQUIRE_EQUAL(sumUse, balance::ByRange::MaxResources * balance::ByRange::MaxLoop);
-}
-*/
